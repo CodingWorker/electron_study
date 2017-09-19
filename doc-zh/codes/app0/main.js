9155f30cf30eec11e043d8adf6aaeb9898ec069e
CodingWorker <dy10010@sina.cn>
2017-09-19 17:35:42 +0800

    add

--- doc-zh/codes/app0/main.js
+++ doc-zh/codes/app0/main.js
@@ -12,19 +12,19 @@
 //});
 // 当 Electron 完成了初始化并且准备创建浏览器窗口的时候 
 // 这个方法就被调用 
-const {app,BrowserWindow}=require('electron');
+const { app, BrowserWindow } = require('electron');
 
 app.on('ready', function() { // 创建浏览器窗口。 
-	mainWindow = new BrowserWindow({
-		width: 800, 
-		height: 600
-	});
-	// 加载应用的 index.html 
-	mainWindow.loadURL('file://' + __dirname + '/index.html');
-	// 打开开发工具 
-	mainWindow.openDevTools();
-	// 当 window 被关闭，这个事件会被发出 
-	mainWindow.on('closed', function() { // 取消引用 window 对象，如果你的应用支持多窗口的话， 通常会把多个 window 对象存放在一个数组里面， 但这次不是。 
-		mainWindow = null; 
-	}); 
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    // 加载应用的 index.html 
+    mainWindow.loadURL('file://' + __dirname + '/html/index.html');
+    // 打开开发工具 
+    mainWindow.openDevTools();
+    // 当 window 被关闭，这个事件会被发出 
+    mainWindow.on('closed', function() { // 取消引用 window 对象，如果你的应用支持多窗口的话， 通常会把多个 window 对象存放在一个数组里面， 但这次不是。 
+        mainWindow = null;
+    });
 });
